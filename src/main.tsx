a41f5fd9ecd17c345b024bdc57c1254fa0a1d3c9
Diego <diegovf@amazon.com>
2025-06-02 21:53:22 +0000

    Deploy com Amplify

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ThemeProvider } from './context/ThemeContext';
+import Amplify from 'aws-amplify';
+import awsExports from '../aws-exports';
+Amplify.configure(awsExports);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
